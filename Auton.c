cf025c1d916e255753fd05c517095f2db935bbba
PRAN1999 <pneeldam@gmail.com>
2017-11-09 17:53:18 -0800

    Added config for auton

--- Auton.c
+++ Auton.c
@@ -1,3 +1,14 @@
+#pragma config(Sensor, dgtl2,  ,               sensorQuadEncoder)
+#pragma config(Sensor, dgtl4,  ,               sensorQuadEncoder)
+#pragma config(Motor,  port1,           claw,          tmotorVex393_HBridge, openLoop, encoderPort, None)
+#pragma config(Motor,  port2,           shoulderR,     tmotorServoContinuousRotation, openLoop, encoderPort, dgtl3)
+#pragma config(Motor,  port3,           shoulderL,     tmotorServoContinuousRotation, openLoop, encoderPort, dgtl4)
+#pragma config(Motor,  port4,           elbowR,        tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port5,           elbowL,        tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port6,           rightDrive,    tmotorServoContinuousRotation, PIDControl, encoderPort, dgtl1)
+#pragma config(Motor,  port7,           leftDrive,     tmotorServoContinuousRotation, openLoop, reversed)
+#pragma config(Motor,  port8,           liftR,         tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port9,           liftL,         tmotorServoContinuousRotation, openLoop)
 
 task main()
 {
